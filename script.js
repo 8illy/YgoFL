0e7c7ee238ba1558e316df837f22850842b9b1e2
unknown <billyhemphill.1997@gmail.com>
2020-05-25 16:11:01 +0100

    removed unneeded sample code from httprequest for cards

--- script.js
+++ script.js
@@ -12,19 +12,12 @@ var cors_api_url = 'https://cors-anywhere.herokuapp.com/';
 	}
 
 	// Bind event - get updated card lit.
-	(function() {
-		var urlField = document.getElementById('url');
-		var dataField = document.getElementById('data');
-		var outputField = document.getElementById('output');
-		document.getElementById('get').onclick =
-		document.getElementById('post').onclick = function(e) {
-			e.preventDefault();
-			doCORSRequest({
-				method: 'GET',
-				url: 'https://www.duelingbook.com/static/cards.json',
-			}, function printResult(result) {
-				cards = JSON.parse(result);
-			});
-		};
+	(function getCardList() {
+		doCORSRequest({
+			method: 'GET',
+			url: 'https://www.duelingbook.com/static/cards.json',
+		}, function printResult(result) {
+			cards = JSON.parse(result);
+		});
 	})();
   
